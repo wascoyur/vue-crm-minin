a9d9aca7fa8a3ed867159c05c7650a5bd236b4a4
wascoyur <mytem20007@gmail.com>
2020-07-07 23:13:21 +0500

    less 2 8:35

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
+  <button class="btn">test</button>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-
+  @import '~materialize-css/dist/css/materialize.min.css';
+  @import 'assets/index.css';
 </style>
