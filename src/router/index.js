8493c74e239ea656cb9203876cff99eebf1bcf52
wascoyur <mytem20007@gmail.com>
2020-07-13 20:40:07 +0500

    3 lesson start

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    meta:{
+      layout: 'main'
+    },
+    component: ()=>import('../views/Home.vue')
   },
   {
     path: '/categories',
@@ -27,20 +29,52 @@ Vue.use(VueRouter)
     component: () => import('../views/Login.vue')
   },
   {
-    path: '/detail',
+    path: '/detail/:id',
     name: 'detail',
     meta: {
-      layout: 'detail'
+      layout: 'main'
     },
     component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {
+      layout: 'main'
+    },
+    component: () => import('../views/Profile.vue')
   },
   {
     path:'/history',
     name:'history',
     meta:{
-      layout:'history'
+      layout:'main'
     },
-    companent: ()=>import('../views/History.vue')
+    component: ()=>import('../views/History.vue')
+  },
+  {
+    path:'/record',
+    name:'record',
+    meta:{
+      layout:'main'
+    },
+    component: ()=>import('../views/Record.vue')
+  },
+  {
+    path:'/register',
+    name:'register',
+    meta:{
+      layout:'empty'
+    },
+    component: ()=>import('../views/Register')
   },
 
 
